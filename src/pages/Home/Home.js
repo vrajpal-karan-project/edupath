70ac79461d74c4a1892e1541327d5e7f30eef491
karan <k123parmar@gmail.com>
2020-04-25 05:18:18 +0530

    Created ItemCard
    Created ItemCard and 
    added Walsheim Font 
    added materialui/lab for Rating
    edited index.css,app.css,Home.js

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Footer from '../../components/Footer';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Grid } from "@material-ui/core";
 import ItemCard from '../../components/ItemCard';
 
 
 const Home = () => {
   return (
-    <div>
+    <div style={{ background: "#F2F2F2" }}>
       <Router>
         {/* <Header /> */}
         <Switch>
@@ -27,9 +28,30 @@ const Home = () => {
               <p>
                 This is COURSES page.
             </p>
-              <div>
-                <ItemCard />
-              </div>
+              <Grid container spacing={3} style={{ transform: "scale(0.9)" }} className="coursesContainer">
+                <Grid item xs={12} sm={6} md={4} lg={3}>
+                  <ItemCard />
+                </Grid>
+                <Grid item xs={12} sm={6} md={4} lg={3}>
+                  <ItemCard />
+                </Grid>
+                <Grid item xs={12} sm={6} md={4} lg={3}>
+                  <ItemCard />
+                </Grid>
+                <Grid item xs={12} sm={6} md={4} lg={3}>
+                  <ItemCard />
+                </Grid>
+                <Grid item xs={12} sm={6} md={4} lg={3}>
+                  <ItemCard />
+                </Grid>
+                <Grid item xs={12} sm={6} md={4} lg={3}>
+                  <ItemCard />
+                </Grid>
+                <Grid item xs={12} sm={6} md={4} lg={3}>
+                  <ItemCard />
+                </Grid>
+
+              </Grid>
             </div>
           </Route>
           <Route exact path="/instructors">

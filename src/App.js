91c08dc2985bf699d103d19dacee0ab94d393297
Vrajpal <jhalavrajpal@gmail.com>
2020-05-01 14:59:16 +0530

    added router and common components to main container

--- src/App.js
+++ src/App.js
@@ -1,12 +1,56 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import { Backdrop } from '@material-ui/core';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from './components/Header';
+import Drawer from './components/Drawer';
+import LoginDialog from './components/LoginDialog';
+import RegisterDialog from './components/RegisterDialog';
+import Footer from './components/Footer';
 import Home from './pages/Home/Home';
+import './App.css';
 
 const App = () => {
+  const [searching, setSearching] = useState(false);
+  const [dialog, setDialog] = useState({});
+  const [drawer, setDrawer] = useState(false);
+
+  const handleDialog = (type, open = true) => {
+    setDialog({ [type]: open });
+    if (drawer)
+      setDrawer(false);
+  };
+
   return (
-    <>
-      <Home />
-    </>
+    <div style={{ background: "#F2F2F2" }}>
+      <Router>
+        <Backdrop open={searching} onClick={() => setSearching(false)} />
+        <Header setDrawer={setDrawer} searching={searching} setSearching={setSearching} handleDialog={handleDialog} />
+        <Drawer drawer={drawer} setDrawer={setDrawer} handleDialog={handleDialog} />
+        <LoginDialog dialog={dialog['login']} handleDialog={handleDialog} />
+        <RegisterDialog dialog={dialog['register']} handleDialog={handleDialog} />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/about">
+            This is ABOUT page.
+         </Route>
+          <Route exact path="/contact">
+            This is CONTACT US page.
+         </Route>
+          <Route exact path="/account">
+            This is ACCOUNT page.
+         </Route>
+          <Route exact path="/courses">
+            This is COURSES page.
+          </Route>
+          <Route exact path="/instructors">
+            This is INSTRUCTORS page.
+         </Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </div>
   );
 }
 
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,91 +1,20 @@
-import React, { useState } from 'react';
-import Header from '../../components/Header';
-import Drawer from '../../components/Drawer';
-import { Backdrop } from '@material-ui/core';
-import LoginDialog from '../../components/LoginDialog';
-import RegisterDialog from '../../components/RegisterDialog';
-import Footer from '../../components/Footer';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Grid } from "@material-ui/core";
+import React from 'react';
+import { Grid, Container } from '@material-ui/core';
 import ItemCard from '../../components/ItemCard';
 
-
 const Home = () => {
-  const [searching, setSearching] = useState(false);
-  const [dialog, setDialog] = useState({});
-  const [drawer, setDrawer] = useState(false);
-
-  const handleDialog = (type, open = true) => {
-    setDialog({ [type]: open });
-    if (drawer)
-      setDrawer(false);
-  };
-
   return (
-    <div style={{ background: "#F2F2F2" }}>
-      <Router>
-        {/* <Header /> */}
-        <Switch>
-          <Route exact path="/">
-            This is home page.
-         </Route>
-          <Route exact path="/about">
-            This is About page.
-         </Route>
-          <Route exact path="/contact">
-            This is Contact US page.
-         </Route>
-          <Route exact path="/account">
-            This is ACCOUNT page.
-         </Route>
-          <Route exact path="/courses">
-            <div>
-              <p>
-                This is COURSES page.
-            </p>
-              <Grid container spacing={3} style={{ transform: "scale(0.9)" }} className="coursesContainer">
-                <Grid item xs={12} sm={6} md={4} lg={3}>
-                  <ItemCard />
-                </Grid>
-                <Grid item xs={12} sm={6} md={4} lg={3}>
-                  <ItemCard />
-                </Grid>
-                <Grid item xs={12} sm={6} md={4} lg={3}>
-                  <ItemCard />
-                </Grid>
-                <Grid item xs={12} sm={6} md={4} lg={3}>
-                  <ItemCard />
-                </Grid>
-                <Grid item xs={12} sm={6} md={4} lg={3}>
-                  <ItemCard />
-                </Grid>
-                <Grid item xs={12} sm={6} md={4} lg={3}>
-                  <ItemCard />
-                </Grid>
-                <Grid item xs={12} sm={6} md={4} lg={3}>
-                  <ItemCard />
-                </Grid>
-
-              </Grid>
-            </div>
-          </Route>
-          <Route exact path="/instructors">
-            This is instructors page.
-         </Route>
-        </Switch>
-
-        <Footer />
-
-      </Router>
-
-    </div>
-    <>
-      <Backdrop open={searching} onClick={() => setSearching(false)} />
-      <Header setDrawer={setDrawer} searching={searching} setSearching={setSearching} handleDialog={handleDialog} />
-      <Drawer drawer={drawer} setDrawer={setDrawer} handleDialog={handleDialog} />
-      <LoginDialog dialog={dialog['login']} handleDialog={handleDialog} />
-      <RegisterDialog dialog={dialog['register']} handleDialog={handleDialog} />
-    </>
+    <Container maxWidth="xl" style={{ background: "silver" }}>
+      <Grid container spacing={3} className="coursesContainer">
+        {
+          [1, 2, 3, 4, 5, 6, 7].map((value) =>
+            <Grid item xs={12} sm={6} md={4} lg={3} key={value}>
+              <ItemCard />
+            </Grid>
+          )
+        }
+      </Grid>
+    </Container>
   );
 }
 

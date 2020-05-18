19fd83e9be007b38e3bf73e13443bfab7f3fca8b
Vrajpal <jhalavrajpal@gmail.com>
2020-05-18 12:47:37 +0530

    Added slider component and tooltip placement feature
    wrap it around components to make them slide, pass "left" or "right" as tooltip prop to ItemCard for position (default none - to disable tooltip)

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Grid, Container } from '@material-ui/core';
 import ItemCard from '../../components/ItemCard';
+import Slider from '../../components/Slider';
 import Banner from './Banner';
 
 const Home = () => {
@@ -12,14 +13,14 @@ const Home = () => {
         <Grid item xs={12} sm={12} md={12} lg={12}>
           This is HOME page.
         </Grid>
-        <Grid container item className="coursesContainer">
-          {
-            [1, 2, 3, 4, 5, 6, 7].map((value) =>
-              <Grid item xs={12} sm={6} md={4} key={value}>
-                <ItemCard tilt={true} tooltip={true} />
-              </Grid>
-            )
-          }
+        <Grid item xs={12}>
+          <Slider>
+            {
+              [1, 2, 3, 4, 5, 6, 7].map((value) =>
+                <ItemCard tooltip={`${value % 3 === 0 ? "left" : "right"}`} key={value} />
+              )
+            }
+          </Slider>
         </Grid>
       </Grid>
     </Container>

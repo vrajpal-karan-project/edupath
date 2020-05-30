39fc15f5f68c3c5d2cd48e56d93a85c6e70121cf
Vrajpal <jhalavrajpal@gmail.com>
2020-05-30 13:31:00 +0530

    Improved design, added pagination and filter feature

--- src/pages/Courses/Category.js
+++ src/pages/Courses/Category.js
@@ -1,53 +1,65 @@
 import React from 'react';
 import {
-    ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails,
-    Checkbox, List, ListItem, ListItemIcon, ListItemText
+  ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails,
+  Checkbox, List, ListItem, ListItemIcon, ListItemText, createMuiTheme, ThemeProvider
 } from '@material-ui/core';
 
+const theme = createMuiTheme({
+  overrides: {
+    MuiExpansionPanelDetails:{
+      root: {
+        paddingTop: 0,
+      },
+    },
+    MuiList: {
+      root: {
+        width: '100%',
+      },
+    },
+  },
+});
 
 const Categories = (props) => {
     const [expanded, setExpanded] = React.useState(false);
 
-    let category = props.category || "Dummy Category";
-    let selected = [...props.selected] || ["Dummy Category"]
+  let category = props.category || "Dummy Category";
+  let selected = [...props.selected] || ["Dummy Category"];
+  const checkedItems = props.checkedItems;
+  const handleCheck = props.handleCheck;
 
-    const [checked, setChecked] = React.useState("");
+  return (
+    <ThemeProvider theme={theme}>
+      <ExpansionPanel TransitionProps={{ unmountOnExit: true }}>
+        <ExpansionPanelSummary expandIcon={<i className="fa fa-angle-down"></i>}>
+          {category}
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <List disablePadding>
+            {[1, 2, 3].map((value, index) => {
+              const labelId = `checkbox-list-label-${value}`;
+              const subcategory = "sub" + category + value;
 
-    const handleToggle = (subcategory) => {
-        setChecked(checked === subcategory ? "" : subcategory);
-    };
-
-    return (
-        <ExpansionPanel>
-            <ExpansionPanelSummary expandIcon={<i className="fa fa-angle-down"></i>}>
-                {category}
-            </ExpansionPanelSummary>
-            <ExpansionPanelDetails>
-                <List>
-                    {[1, 2, 3].map((value) => {
-                        const labelId = `checkbox-list-label-${value}`;
-                        const subcategory = "sub" + category + value;
-                        return (
-                            <ListItem key={value} role={undefined} dense button onClick={() => handleToggle(subcategory)}>
-                                <ListItemIcon>
-                                    <Checkbox
-                                        edge="start"
-                                        checked={checked === subcategory}
-                                        name={subcategory}
-                                        tabIndex={-1}
-                                        disableRipple
-                                        onChange={() => handleToggle(subcategory)}
-                                    />
-                                </ListItemIcon>
-                                <ListItemText id={labelId} primary={subcategory} />
-
-                            </ListItem>
-                        );
-                    })}
-                </List>
-            </ExpansionPanelDetails>
-        </ExpansionPanel>
-    )
+              return (
+                <ListItem key={index} role={undefined} dense button onClick={() => handleCheck(subcategory)}>
+                  <ListItemIcon>
+                    <Checkbox
+                      edge="start"
+                      checked={Boolean(checkedItems[subcategory])}
+                      name={subcategory}
+                      tabIndex={-1}
+                      disableRipple
+                      onChange={() => handleCheck(subcategory)}
+                    />
+                  </ListItemIcon>
+                  <ListItemText id={labelId} primary={subcategory} />
+                </ListItem>
+              );
+            })}
+          </List>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+    </ThemeProvider>
+  )
 }
 
 export default Categories
--- src/pages/Courses/Courses.js
+++ src/pages/Courses/Courses.js
@@ -1,62 +1,138 @@
-import React from 'react';
-import { Grid, Container, Select, MenuItem } from '@material-ui/core';
+import React, { useState } from 'react';
+import {
+  Grid,
+  Container,
+  Select,
+  MenuItem,
+  Typography,
+  makeStyles,
+  createMuiTheme,
+  ThemeProvider,
+  darken
+} from '@material-ui/core';
+import { Pagination } from '@material-ui/lab';
 import ItemCard from '../../components/ItemCard';
 import Category from "./Category";
 
+const theme = createMuiTheme({
+  overrides: {
+    MuiPagination: {
+      root: {
+        display: 'flex',
+        justifyContent: 'center',
+        padding: `30px 0px 10px 0px`,
+      },
+    },
+    MuiPaginationItem: {
+      root: {
+        color: '#25274D',
+      },
+      page: {
+        '&:hover, &.Mui-focusVisible': {
+          backgroundColor: '#5CDB94',
+        },
+        '&.Mui-selected': {
+          backgroundColor: '#5CDB94',
+          '&:hover, &.Mui-focusVisible': {
+            backgroundColor: `${darken('#5CDB94', .15)} !important`,
+          }
+        },
+      },
+    },
+  }
+});
+
+const useStyle = makeStyles(theme => ({
+  pageTitle: {
+    color: '#25274D',
+    fontWeight: 'bolder',
+  },
+}));
+
 const Courses = (props) => {
-    const [sortBy,setSortBy] = React.useState("alpha");
-    const handleSortBy=(e)=>{
-        setSortBy(e.target.value);
-    }
-    return (
-        <Container maxWidth="xl" style={{ margin: "24px auto" }}>
-            <Grid container>
-                <Grid item xs={12}>
-                    This is Courses page.
-                </Grid>
-                <Grid item container xs={12} justify="center">
-                    <Grid container item xs={12} md={3}  style={{ padding:"12px" }}>
-                        <Grid item xs={12}>
-                            CATEGORIES
-                            {
-                                ["IT","Programming","Software","Hardware","Android"].map((cat,i)=>
-                                    <Category category={cat} selected={["IT","Software"]} key={i}/>
-                                )
-                            }
-                        </Grid>
-                    </Grid>
-                    <Grid container item xs={12} md={9} justify="center" style={{paddingLeft:"16px"}}>
-                        <Grid container item xs={12} justify="flex-end" style={{ padding: "12px" }}>
-                            <Grid item>
-                                SORT BY:
-                              
-                                <Select
-                                    value={sortBy}
-                                    displayEmpty
-                                    inputProps={{ 'aria-label': 'Sort B' }}
-                                    onChange={handleSortBy} 
-                                     style={{minWidth:"18ch", textAlign:"right"}}
-                                >
-                                    <MenuItem value={"alpha"}>A-Z</MenuItem>
-                                    <MenuItem value={"date"}>Date Uploaded</MenuItem>
-                                    <MenuItem value={"price"}>Price Low To High</MenuItem>
-                                </Select>
-                            </Grid>
-                        </Grid>
-                        <Grid container item xs={12} className="coursesContainer">
-                            {
-                                [1, 2, 3, 4, 5, 6, 7].map((value) =>
-                                    <Grid item xs={12} sm={6} md={4} key={value}>
-                                        <ItemCard />
-                                    </Grid>
-                                )
-                            }
-                        </Grid>
-                    </Grid>
-                </Grid>
+  const classes = useStyle();
+
+  const [sortBy, setSortBy] = useState("alpha");
+  const [checkedItems, setCheckedItems] = useState({});
+
+  const handleSortBy = (e) => {
+    setSortBy(e.target.value);
+  }
+
+  const handleCheck = (subcategory) => {
+    const checkList = { ...checkedItems };
+
+    if (checkList[subcategory] === undefined)
+      checkList[subcategory] = subcategory;
+    else
+      delete checkList[subcategory];
+
+    setCheckedItems(checkList);
+
+    console.log(Object.entries(checkList).map(([k, v]) => (v)));
+  };
+
+  return (
+    <Container maxWidth="xl" style={{ margin: "24px auto" }}>
+      <Grid container>
+        <Grid item xs={12}>
+          <Typography variant="h5" className={classes.pageTitle}>
+            All Courses
+          </Typography>
+        </Grid>
+        <Grid item container xs={12} alignItems="flex-start">
+          <Grid container item xs={12} md={3} style={{ padding: "12px" }}>
+            <Grid item xs={12}>
+              {
+                ["IT", "Programming", "Software", "Hardware", "Android"].map((cat, i) =>
+                  <Category category={cat} selected={["IT", "Software"]} checkedItems={checkedItems} handleCheck={handleCheck} key={i} />
+                )
+              }
             </Grid>
-        </Container>
-    );
+          </Grid>
+          <Grid container item xs={12} md={9} justify="center" style={{ paddingLeft: "16px" }}>
+            <Grid container item xs={12} justify="flex-end" style={{ padding: "12px" }}>
+              <Grid item>
+                SORT BY:
+                <Select
+                  value={sortBy}
+                  displayEmpty
+                  inputProps={{ 'aria-label': 'Sort B' }}
+                  onChange={handleSortBy}
+                  style={{ minWidth: "18ch", textAlign: "right" }}
+                >
+                  <MenuItem value={"alpha"}>A-Z</MenuItem>
+                  <MenuItem value={"date"}>Date Uploaded</MenuItem>
+                  <MenuItem value={"price"}>Price Low To High</MenuItem>
+                </Select>
+              </Grid>
+            </Grid>
+            <Grid container item xs={12}>
+              {
+                [1, 2, 3, 4, 5, 6, 7].map((value, index) =>
+                  <Grid item xs={12} sm={6} md={4} key={index}>
+                    <ItemCard tilt={true} tooltip={true} index={index} />
+                  </Grid>
+                )
+              }
+            </Grid>
+            <Grid item xs={12}>
+              <ThemeProvider theme={theme}>
+                <Pagination
+                  className={classes.pagination}
+                  classes={{ ul: classes.paginationItem }}
+                  count={10}
+                  size="large"
+                  showFirstButton
+                  showLastButton
+                />
+              </ThemeProvider>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
 
 export default Courses;
